0e8be8f544dfd5b1a34a56294dba9c57d46c946b
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2022-02-09 15:42:45 -0500

    Revised posting method from being a local copy to pushing to the express server

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -14,12 +14,11 @@ export class PostsService {
     //expects a path (will be made more dynamic later)
     //can specify this generic git what it should expect
     //Get will also our JSON object out and there no need to recast it
-    this.http.get<{message: string, posts: Post[]}>("http://localhost:3000/api/posts")
+    this.http.get<{message: string, posts: Post[]}>("http://localhost:3000/api/get")
       .subscribe((postData) => {
         this.posts = postData.posts;
         this.postsUpdated.next([...this.posts]);
       });//need something to listen to it
-    
   }
 
   getPostUpdateListener() {
@@ -28,7 +27,14 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = {id: null, title: title, content: content};
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    this.http
+      .post<{message: string}>("http://localhost:3000/api/posts", post)
+        .subscribe((responseData) => {
+          console.log(responseData.message);
+          //this is moved due to the subscribe will only work when asynchronous post returns success.
+          this.posts.push(post);//local data which is fine to keep since we will probably want to mess with it on the client side
+          this.postsUpdated.next([...this.posts]);
+        });
   }
+
 }
