b8191ae90a7976524546fb2cfc9d9d9991e5c9e6
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2022-02-09 14:59:56 -0500

    Set HEADERS to prevent CORS error

--- server/app.js
+++ server/app.js
@@ -2,6 +2,19 @@ const express = require("express");
 
 const app = express();
 
+/**
+ * Set up middle ware to handle the header. No path filters specified so ALL requests to Express
+ * will hit this function.
+ */
+app.use((request, response, next) => {
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Headers", 
+    "Origin, X-Requested-With, Content-Type, Accept");
+    response.setHeader("Access-Control-Allow-Methods", 
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+    next();//all request should be allowed to continue to the next methods.
+});
+
 app.use("/api/posts", (req, res, next) => {
     //later will actually be from 
     const posts = [
