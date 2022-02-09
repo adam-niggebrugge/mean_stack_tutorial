e0979c3ba87547f00be6518300d5ca0baa77c53a
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2022-02-09 15:33:50 -0500

    Adding HTTPClientModule to allow angular access to REST controls in services or components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http"; //This angular module unlocks the ability for components/services to now handle REST API commands
 import {
   MatInputModule,
   MatCardModule,
