68a0d06f0fb5b41af4edcb917f7ed4fe3e863522
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2022-02-09 13:57:25 -0500

    Modified angular app to subscribe to a service to retrieve data from the back end

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
-
+import { HttpClient } from '@angular/common/http';
 import { Post } from './post.model';
 
 @Injectable({providedIn: 'root'})
@@ -8,8 +8,18 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient,) {}
+
   getPosts() {
-    return [...this.posts];
+    //expects a path (will be made more dynamic later)
+    //can specify this generic git what it should expect
+    //Get will also our JSON object out and there no need to recast it
+    this.http.get<{message: string, posts: Post[]}>("http://localhost:3000/api/posts")
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts]);
+      });//need something to listen to it
+    
   }
 
   getPostUpdateListener() {
@@ -17,7 +27,7 @@ export class PostsService {
   }
 
   addPost(title: string, content: string) {
-    const post: Post = {title: title, content: content};
+    const post: Post = {id: null, title: title, content: content};
     this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
   }
